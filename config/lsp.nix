ae81ecdc612f4e01b4efe29f3f457799f1863922
Monish Yeluguri <willoh.y@protonmail.com>
2025-02-17 10:16:53 -0500

    nix file lsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,6 +3,14 @@
     enable = true;
     servers = {
       lua_ls.enable = true;
+      nil_ls = {
+            enable = true;
+          settings = {
+            formatting = {
+              command = [ "nixfmt" ];
+            };
+      };
     };
   };
+};
 }
